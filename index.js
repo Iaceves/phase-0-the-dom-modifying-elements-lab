0849ba6d2174c2b59559ac0c7bc9d038c270ee6e
iaceves <acevesaceves97@gmail.com>
2022-07-30 20:07:51 -0400

    commit tree 50216aaddd7082d84b708aa887f07f8933dda22b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+//select element by id
+const main = document.getElementById("main");
+main.remove()
+const newHeader = document.createElement("H1");
+newHeader.id = "victory";
+newHeader.textContent = "Ismael is the champion";
